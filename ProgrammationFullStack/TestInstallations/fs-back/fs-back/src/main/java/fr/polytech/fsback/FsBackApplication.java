a2aa5fcd47e495d72ab33216654931a720dae326
Juliette <77498912+Juju-sail@users.noreply.github.com>
2022-09-07 21:08:00 +0200

    progFullStackSeance1

---
+++ ProgrammationFullStack/TestInstallations/fs-back/fs-back/src/main/java/fr/polytech/fsback/FsBackApplication.java
@@ -0,0 +1,19 @@
+package fr.polytech.fsback;
+
+import java.util.ArrayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsBackApplication {
+//--------------------
+// MAIN DU PROJET :
+//--------------------
+	public static void main(String[] args) {
+		SpringApplication.run(FsBackApplication.class, args);
+		
+
+	}
+
+}
---
+++ ProgrammationFullStack/TestInstallations/fs-back/fs-back/src/main/java/fr/polytech/fsback/services/LivreService.java
@@ -0,0 +1,28 @@
+package fr.polytech.fsback.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import fr.polytech.fsback.dto.LivreDto;
+
+@Service
+public class LivreService {
+	public List<LivreDto> listeDeLivres = new ArrayList<LivreDto>();
+	
+	
+	public LivreService() {
+		LivreDto l1 = new LivreDto((int) (Math.random()*100), "gtr");
+		LivreDto l2 = new LivreDto((int) (Math.random()*100), "fghj");
+		this.listeDeLivres.add(l1);
+		this.listeDeLivres.add(l2);
+	}
+	
+	
+	public LivreDto getLivreById(int id) {
+		return this.listeDeLivres.stream().filter(livreDto -> livreDto.getId()==id).findFirst().get();
+	}
+
+	
+}
