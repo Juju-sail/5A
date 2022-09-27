86ba10a7b4b5938edf930a67ee5cebb0c7e32375
Juliette <77498912+Juju-sail@users.noreply.github.com>
2022-09-27 15:57:17 +0200

    IA_TP3_finSeance1

--- IA/TP3/kppvmnist.py
+++ IA/TP3/kppvmnist.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.utils import check_random_state
-	
+from sklearn import neighbors
 
 
 
