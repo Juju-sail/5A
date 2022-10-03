4b5e6c3d28724df5db8933758e1bf8f78e83aaf3
Juliette <77498912+Juju-sail@users.noreply.github.com>
2022-10-03 15:52:05 +0200

    IA_TP4_fin

--- IA/TP4/classBinaires.py
+++ IA/TP4/classBinaires.py
@@ -33,7 +33,7 @@ for i in range(4):
     Err = np.mean(Ypred != yi)*100
     print(Err)
 
-    # On transforme nos 4 models en 1 :
+    # On transforme nos 4 models en 1, on les combine :
     G[:,i] = svmtab[i].decision_function(X)
 
 # notre y pred du model global
@@ -43,26 +43,19 @@ ypred.append(np.argmax(G, axis=1) + 1)
 # Et son erreur
 Err = np.mean(ypred != y)*100
 print(Err)
-# ------------------------------------------------------------------------
+
 # LOO :
-ypred = []
+Gbis = np.zeros((139,4))
 for i in range(140):
     X_i = np.delete(X, i, axis=0)
     y_i = np.delete(y, i)
 
     for j in range(4):
         yi = 2*(y_i==(j+1))-1
-        
         svmtab[j].fit(X_i, yi)
-        Ypred = svmtab[j].predict(X[i,:])
-        
-        Err = np.mean(Ypred != yi)*100
-
-        G[i,j] = svmtab[j].decision_function(X_i)
-    
-    
-    ypred.append(np.argmax(G[i,:], axis=1) + 1)
-    Err = np.mean(ypred != y_i)*100
-Erreur = np.sum(Err)      
-print(Erreur)
+        yPrevision = svmtab[j].predict(X_i)
+        Gbis[:,j] = svmtab[j].decision_function([X[i,:]])
+        Err = np.mean(yPrevision != yi)*100
+Erreur = np.mean(Err)
+print(Erreur)   
 # A modif des que possible
