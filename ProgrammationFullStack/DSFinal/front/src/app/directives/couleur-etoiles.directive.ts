f43569fe17b425b9014ee08eaf07ef512bb5e802
Juliette <77498912+Juju-sail@users.noreply.github.com>
2022-10-05 17:58:07 +0200

    PFS_DS_directiveCrée

--- ProgrammationFullStack/DSFinal/front/src/app/directives/couleur-etoiles.directive.ts
+++ ProgrammationFullStack/DSFinal/front/src/app/directives/couleur-etoiles.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appCouleurEtoiles]'
 })
 export class CouleurEtoilesDirective {
 
-  constructor() { }
+  @Input()
+  set index(index: string) {
+
+    if (parseInt(index) > 2) {
+      this._renderer.setStyle(this._el.nativeElement, 'color', 'yellow')
+    } else {
+      if (parseInt(index) < 1 && parseInt(index) > 0) {
+        this._renderer.setStyle(this._el.nativeElement, 'color', 'red')
+      } else {
+        this._renderer.setStyle(this._el.nativeElement, 'color', 'black')
+      }
+    }
+
+  }
+
+  constructor(private _el: ElementRef, private _renderer: Renderer2) {
+
+  }
 
 }
