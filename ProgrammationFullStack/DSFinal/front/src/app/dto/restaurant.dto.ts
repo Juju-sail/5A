c4bfed2e3f6d3abc5924b5312c172bea8ac43508
Juliette <77498912+Juju-sail@users.noreply.github.com>
2022-10-05 16:58:57 +0200

    PFS_DS_FormOK

--- ProgrammationFullStack/DSFinal/front/src/app/dto/restaurant.dto.ts
+++ ProgrammationFullStack/DSFinal/front/src/app/dto/restaurant.dto.ts
@@ -1,7 +1,9 @@
+import {EvaluationComponent} from "../components/evaluation/evaluation.component";
+
 export interface Restaurant {
   nom: string;
   adresse: string;
   moyenne: number;
-
+  evaluations: EvaluationComponent[];
 }
 
