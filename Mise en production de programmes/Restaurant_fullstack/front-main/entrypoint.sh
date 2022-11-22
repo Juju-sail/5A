d2e9dcd79822e0e7319365cd37c2c94b8d95110e
Juliette <77498912+Juju-sail@users.noreply.github.com>
2022-11-22 14:56:20 +0100

    MiseProductionProgrammes_Seance3

---
+++ Mise en production de programmes/Restaurant_fullstack/front-main/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sed -i 's/{{ BACKEND_URL }}/$BACKEND_URL_ENV_VAR/g' /usr/share/nginx/html/*
+
+exec "$@"
--- ProgrammationFullStack/Back/TestInstallations/fs-back/fs-back/src/main/java/fr/polytech/fsback/exceptions/LivresNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package fr.polytech.fsback.exceptions;
-
-public class LivresNotFoundException extends RuntimeException{
-	public LivresNotFoundException(String message) {
-		super(message);
-	}
-}
