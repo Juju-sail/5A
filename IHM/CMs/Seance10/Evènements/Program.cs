5f3a4fe30aec073ecf82e1933d2cc5c59756c08a
Juliette <77498912+Juju-sail@users.noreply.github.com>
2023-02-26 18:00:35 +0100

    IHM_Seance10 | Objets observables

---
+++ IHM/CMs/Seance10/Evènements/Program.cs
@@ -0,0 +1,58 @@
+﻿
+ConsoleObservable o = new ConsoleObservable(new SansEspacesObservateur(new MajusculesObservateur()));
+o.Demarre();
+
+class MajusculesObservateur : IConsoleObservateur
+{
+    public void NouvelleLigne(string texte)
+    {
+        Console.WriteLine(texte.ToUpper());
+    }
+}
+
+class SansEspacesObservateur : IConsoleObservateur
+{
+    private readonly IConsoleObservateur _observateur;
+
+    public SansEspacesObservateur(IConsoleObservateur observateur)
+    {
+        _observateur = observateur;
+    }
+
+    public void NouvelleLigne(string texte)
+    {
+        _observateur.NouvelleLigne(texte.Replace(" ", ""));
+    }
+}
+
+
+#region ConsoleObservable / IConsoleObservateur
+
+class ConsoleObservable
+{
+    private readonly IConsoleObservateur _observateur;
+
+    public ConsoleObservable(IConsoleObservateur observateur)
+    {
+        _observateur = observateur;
+    }
+
+    public void Demarre()
+    {
+        for (;;) // while (true)
+        {
+            string? texte = Console.ReadLine();
+            if (texte != null)
+            {
+                _observateur.NouvelleLigne(texte);
+            }
+        }
+    }
+}
+
+interface IConsoleObservateur
+{
+    void NouvelleLigne(string texte);
+}
+
+#endregion
