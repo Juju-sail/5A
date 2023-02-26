ebc1825bccd5a86e3dd636170c88010812efa503
Juliette <77498912+Juju-sail@users.noreply.github.com>
2023-02-26 17:51:39 +0100

    IHM_Seance4 | Decoupe en projets

--- IHM/CMs/Séance4/Formulaire.ViewModel/obj/Debug/net7.0/Formulaire.ViewModel.AssemblyInfo.cs
+++ IHM/CMs/Séance4/Formulaire.ViewModel/obj/Debug/net7.0/Formulaire.ViewModel.AssemblyInfo.cs
@@ -11,15 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Formulaire")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Formulaire.ViewModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Formulaire")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Formulaire")]
+[assembly: System.Reflection.AssemblyProductAttribute("Formulaire.ViewModel")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Formulaire.ViewModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
-[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
